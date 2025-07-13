5cdcd6e3657d71388b836dc931926b71ee545ce0
yeudaborodyanski <yeudaborodyanski@gmail.com>
2025-07-13 07:28:40 +0300

    Initial Commit

---
+++ CopyFastTests/CopyFastTests.swift
@@ -0,0 +1,17 @@
+//
+//  CopyFastTests.swift
+//  CopyFastTests
+//
+//  Created by Yeuda Borodyanski on 13/07/2025.
+//
+
+import Testing
+@testable import CopyFast
+
+struct CopyFastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
